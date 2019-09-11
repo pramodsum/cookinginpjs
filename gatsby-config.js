06acc0e947cf5d27e380f2e9ba864bb48d112070
Sumedha Pramod <sumedha.pramod@gmail.com>
2019-09-11 12:24:26 -0700

    Remove serviceworker

--- gatsby-config.js
+++ gatsby-config.js
@@ -212,6 +212,6 @@ module.exports = {
     `gatsby-plugin-catch-links`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-force-trailing-slashes`,
-    `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
   ],
 };
