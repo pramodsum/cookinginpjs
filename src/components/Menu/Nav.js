5e4605c16a25eeeb5f43b09c9b4df23e88735fe0
Sumedha Pramod <sumedha.pramod@gmail.com>
2019-10-27 16:07:13 -0700

    Fix mobile header padding

--- src/components/Menu/Nav.js
+++ src/components/Menu/Nav.js
@@ -7,27 +7,34 @@ import bgy from '../../assets/bgyl.png';
 const CategoryList = styled.ul`
   display: flex;
   justify-content: space-between;
-  padding: ${props => (props.collapsed ? '0' : '8px 0 0')};
+  padding: '8px 0 0';
   margin-block-start: ${props => (props.collapsed ? '0' : '.5rem')};
   margin-block-end: ${props => (props.collapsed ? '0' : '.5rem')};
 
-  @media screen and (max-width: 960px) : {
+  @media screen and (max-width: 768px) {
     flex-direction: column;
     border-top: 1px solid lightgray;
+    padding: 0;
+    /* padding: ${props => (props.collapsed ? '0' : '8px 0 0')}; */
   }
 `;
 
 const CategoryWrapper = styled.li`
   list-style: none;
-  padding: ${props => (props.collapsed ? '.35rem' : '.85rem')};
+  padding: '.35rem';
 
-  @media screen and (max-width: 960px): {
+  @media screen and (max-width: 768px) {
     border-bottom: 1px solid lightgray;
-  },
+    padding: ${props => (props.collapsed ? '.35rem' : '.85rem .85rem .65rem')};
+  }
 
-  &:hover: {
-    background: url(${bgy}) repeat,
-  },
+  @media screen and (max-width: 375px) {
+    padding: ${props => (props.collapsed ? '.35rem' : '.85rem .85rem .95rem')};
+  }
+
+  &:hover {
+    background: url(${bgy}) repeat;
+  }
 `;
 
 const Category = styled(Link)`
